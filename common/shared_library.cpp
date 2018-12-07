f8fa4ddda735b8466bcb6381b6701142917429e9
Novate Z <22500949+novate@users.noreply.github.com>
2018-12-07 23:31:42 +0800

    Update shared_library.cpp

--- common/shared_library.cpp
+++ common/shared_library.cpp
@@ -1,13 +1,11 @@
 #include "shared_library.hpp"
 
-
 int sig_cksum_err              =   false;
 int sig_frame_arrival          =   false;
 int sig_network_layer_ready    =   false;
 int sig_enable_network_layer   =   false;
 int sig_timeout                =   false;
 int sig_ack_timeout            =   false;
-
 
 /*****************************/
 /*****  Network Layer   ******/
@@ -1074,391 +1072,6 @@ Status tcp_recv(const int socket, char *buf_recv, const bool is_data) {
     }
 }
 
-/*
-Status sender_physical_layer_old(int *pipefd_down, int *pipefd_up) {
-    prctl(PR_SET_PDEATHSIG, SIGHUP);
-    //fd_set rfds, wfds;
-    int client_fd = tcp_client_block();
-    LOG(Debug) << "[SPL] client_fd\t" << client_fd << endl;
-    if (client_fd < 0) {
-        LOG(Error) << "[SPL] An error occured when initializing TCP client socoket or connect with error code: " << client_fd << endl;
-        return client_fd;
-    }
-
-    int flags;
-    flags = fcntl(client_fd, F_GETFL, 0);
-    fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
-
-    close(pipefd_down[p_write]);
-    if(pipefd_up) {
-        close(pipefd_up[p_read]);
-    }
-
-    int r_rtn, w_rtn;
-    unsigned int *r_seq;
-    frame r;
-    char buffer[LEN_PKG_DATA] = {0};
-    Status val_tcp_send;
-    Status val_tcp_recv;
-    int flag_trans_end = false;
-    int flag_sleep = true;
-
-    while(1) {
-        flag_sleep = true;
-        //nonblock
-        r_rtn = read(pipefd_down[p_read], buffer, LEN_PKG_DATA);
-        if(r_rtn <= 0 && errno != EAGAIN){
-            LOG(Error) << "[SPL] Pipe read from SNL to SDL error" << endl;
-            return E_PIPE_READ;
-        }
-        LOG(Debug) << "[SPL] Read info from SNL: " << buffer << endl;
-        //if r_rtn == -1 and errno == EAGAIN, mean temporarily no data to read, just don't read this time
-        if (r_rtn > 0) {
-            flag_sleep = false;
-
-            FD_CLR(client_fd, &wfds);
-            if (0 != memcmp(all_zero, buffer+LEN_PKG_NODATA, RAW_DATA_SIZE)) {
-                val_tcp_send = tcp_send(client_fd, buffer);
-            } else {  //transmission end
-                LOG(Info) << "[SPL] Transmission end, detected by SPL" << endl;
-                val_tcp_send = tcp_send(client_fd, buffer, true, true);
-                flag_trans_end = true;
-            }
-            LOG(Debug) << "[SPL] val_tcp_send\t" << val_tcp_send << endl;
-            if (val_tcp_send < 0) {
-                LOG(Error) << "[SPL] An error occured, val_tcp_send code: " << val_tcp_send << endl;
-                return val_tcp_send;
-            }
-        }//end of if read > 0
-
-        if(pipefd_up) {
-            //expecting to recv ACK/NAK
-            val_tcp_recv = tcp_recv(client_fd, buffer, false);
-            //nothing received
-            if(val_tcp_recv == E_RECV) {
-                continue;
-            }
-            //other error: ignore this packet
-            else if(val_tcp_recv < 0) {
-                continue;
-            }
-            else {    //recved frame from receiver: ACK/NAK
-                flag_sleep = false;
-                LOG(Debug) << "[SPL] Get info from SPL: " << buffer << endl;
-                    
-                //to test if recved ACK/NAK frame
-                r_seq = (unsigned int *)(buffer+4);
-                if((*r_seq) != 0xFFFFFFFF){
-                    LOG(Error) << "[SPL] received unkown frame type!" <<endl;
-                    continue;
-                }
-                //recognized ACK/NAK
-                //set buffer+12 ~ buffer+1035 all zero
-                memcpy(buffer+12, all_zero, RAW_DATA_SIZE);
-                while(1){
-                    w_rtn = write(pipefd_up[p_write], buffer, LEN_PKG_DATA);
-                    if(w_rtn <= 0 && errno != EAGAIN){
-                        LOG(Error) << "[SPL] Pipe write to SDL error" << endl;
-                        return E_PIPE_WRITE;
-                    }
-                    if(w_rtn > 0)
-                        break;
-                    //w_rtn < 0 &&errno == EAGAIN, try again
-                }
-                kill(getppid(), SIGFRARV);
-            }//end of else
-
-            //after recv ACK
-            if(flag_trans_end == true) {
-                break;
-            }
-        }
-        else {
-            if(flag_trans_end == true) {
-                break;
-            }
-        }
-    
-        //if nothing happened in this loop
-        if(flag_sleep == true) {
-            usleep(5);
-        }
-    }//end of while
-
-    //detected transmission_end
-    close(pipefd_down[p_read]);
-    if(pipefd_up)
-        close(pipefd_up[p_write]);
-    // THINK: do we need to sleep here? How long do we need to sleep?
-    //sleep(1);
-    // THINK: can client_fd be closed here?
-    while(1) {
-        if (0 == send(client_fd, NULL, 0, 0)) 
-            break;
-        else 
-            sleep(1);
-    }
-    LOG(Info) << "[SPL] peer(RPL) disconnected detected, SPL will end too." << endl;
-    close(client_fd);
-    return TRANSMISSION_END;
-}
-
-Status receiver_physical_layer_old(int *pipefd_down, int *pipefd_up) {
-    prctl(PR_SET_PDEATHSIG, SIGHUP);
-    fd_set rfds, wfds;
-    int server_fd = tcp_server_block();
-    LOG(Debug) << "[RPL] server_fd\t" << server_fd << endl;
-    if (server_fd < 0) {
-        LOG(Error) << "[RPL] An error occured when initializing TCP server socoket or connect with error code: " << server_fd << endl;
-        return server_fd;
-    }
-
-    int flags;
-    flags = fcntl(server_fd, F_GETFL, 0);
-    fcntl(server_fd, F_SETFL, flags | O_NONBLOCK);
-
-    close(pipefd_up[p_read]);
-    if(pipefd_down)
-        close(pipefd_down[p_write]);
-
-    int r_rtn, w_rtn;
-    frame s;
-    char buffer[LEN_PKG_DATA] = {0};   
-    Status val_tcp_recv;
-    Status val_tcp_send;
-    int flag_trans_end = false;
-    int flag_sleep = true;
-
-    while(1) {
-        flag_sleep = true;
-        //nonblock
-        FD_ZERO(&rfds);     
-        FD_SET(server_fd, &rfds);  
-        if((select(server_fd+1, &rfds, NULL, NULL, NULL)) == -1){
-            LOG(Info) << "[RPL] An error occured, select error." <<endl;
-            return E_SELECT;
-        }
-        if(FD_ISSET(server_fd, &rfds)){
-            FD_CLR(server_fd, &rfds);
-            val_tcp_recv = tcp_recv(server_fd, buffer);   //is_data = true
-            LOG(Debug) << "[RPL] val_tcp_recv\t" << val_tcp_recv << endl;
-
-            //switch(recv < 0):
-            //case(E_RECV): nothing recved, do nothing
-            //case(other error): ignore this packet
-            if(val_tcp_recv >= 0){
-                flag_sleep = false;
-                LOG(Debug) << "[RPL] Get info from SPL: " << buffer << endl;
-
-                while(1){
-                    w_rtn = write(pipefd_up[p_write], buffer, LEN_PKG_DATA); 
-                    if(w_rtn <= 0 && errno != EAGAIN){
-                        LOG(Error) << "[RPL] Pipe write to SDL error" << endl;
-                        return E_PIPE_WRITE;
-                    }
-                    if(w_rtn > 0)
-                        break;
-                    //w_rtn < 0 &&errno == EAGAIN, try again
-                }
-                kill(getppid(), SIGFRARV);
-
-                if (val_tcp_recv == TRANSMISSION_END)
-                    flag_trans_end = true;           
-            }//end of if recv >= 0
-        }
-
-        if(pipefd_down){
-            r_rtn = read(pipefd_down[p_read], buffer, LEN_PKG_DATA);
-            if(r_rtn <= 0 && errno != EAGAIN){
-                LOG(Error) << "[RPL] Pipe read from SNL to SDL error" << endl;
-                return E_PIPE_READ;
-            }
-            //send ACK
-            if(r_rtn > 0){
-                flag_sleep = false;
-                memcpy(buffer, &(s.kind), sizeof(int));
-                memcpy(buffer+4, &(s.seq), sizeof(int));
-                memcpy(buffer+8, &(s.ack), sizeof(int));
-
-                FD_ZERO(&wfds);     
-                FD_SET(server_fd, &wfds);  
-                if((select(server_fd+1, NULL, &wfds, NULL, NULL)) == -1){
-                    LOG(Info) << "[RPL] An error occured, select error." <<endl;
-                    return E_SELECT;
-                }
-                if(FD_ISSET(server_fd, &wfds)){
-                    FD_CLR(server_fd, &wfds);
-                    val_tcp_send = tcp_send(server_fd, buffer, false);
-                    LOG(Debug) << "[RPL] val_tcp_send\t" << val_tcp_send << endl;
-                    
-                    if (val_tcp_send < 0) {
-                        LOG(Error) << "[rPL] An error occured, val_tcp_send code: " << val_tcp_send << endl;
-                        return val_tcp_send;
-                    }
-                    if(flag_trans_end)
-                        break;
-                }
-            }//end of r_rtn > 0
-        }//end of if pipefd_down
-        else             
-            if(flag_trans_end)
-                break;
-        //if nothing happened in this loop
-        if(flag_sleep == true)
-            usleep(1);
-    }//end of while
-
-    LOG(Info) << "[RPL] Transmission end, detected by RPL" << endl;
-    close(pipefd_up[p_write]);
-    close(pipefd_down[p_read]);
-    // THINK: can server_fd be closed here?
-    close(server_fd);
-    LOG(Info) << "[RPL] Wait for RDL's death." << endl;
-    while(1) {
-        sleep(1);
-    }
-    return TRANSMISSION_END;
-}
-*/
-
-// template <class T, class t>
-// typename T::iterator find_by_second_in_list(const T &l, const t second) {
-//     // a helper function that finds in list l with element type of pair by the value of 
-//     // the second component of its pair elements
-//     return find_if(l.begin(), l.end(), [&first](const auto &p) { return p.first == first; });
-// }
-
-/*
-void ticking_handler(int sig) {
-    if ((--(*(timer_list.begin())).first)) == 0) {
-        timer_list.pop_front();
-    }
-}
-
-void _start_timer(seq_nr k) {
-    timer_list.emplace_back(tick_interval, k);
-}
-
-void _stop_timer(seq_nr k) {
-    timer_list.remove_if([&k](const auto &el) { return el.second == k; } );
-}
-*/
-
-/*
-int recv_thing(const int socketfd, char *buffer, const Options &opt, const int recv_len) {
-    int val_recv_ready, val_recv, total_recv;
-
-    memset(buffer, 0, sizeof(char)*buffer_len);
-
-    total_recv = 0;
-    while (total_recv < recv_len) {
-        errno = 0;
-        val_recv_ready = ready_to_recv(socketfd, opt);
-        if (val_recv_ready < 0) {
-            if (val_recv_ready == -1) {
-                return -1;
-            }
-            else if (val_recv_ready == -2) {
-                return -2;
-            }
-            else if (val_recv_ready == -3) {
-                return -8;
-            }
-            else {
-                graceful_return("ready_to_recv", -9);
-            }
-        }
-        val_recv = recv(socketfd, buffer+total_recv, max_sendlen, 0);
-        if (val_recv < 0) {
-            cout << "DEBUG: total_recv: " << total_recv << ", val_recv: " << val_recv << endl;
-            graceful_return("recv", -10);
-        }
-        else if (val_recv == 0) {
-            graceful_return("peer offline", -3);
-        }
-        else {
-            total_recv += val_recv;
-        }
-    }
-    if (total_recv != recv_len) {
-        graceful_return("not received exact designated quantity of bytes", -10);
-    }
-    return 0;       // all good
-}
-*/
-
-int ready_to_send(int socketfd) {
-    fd_set readfds, writefds;
-    struct timeval tv;
-    FD_ZERO(&readfds);
-    FD_ZERO(&writefds);
-    //FD_SET(socketfd, &readfds);
-    FD_SET(socketfd, &writefds);
-    tv.tv_sec = timeout_seconds;
-    tv.tv_usec = timeout_microseconds;
-    errno = 0;  // clear errno
-
-    int val_select = select(socketfd+1, &readfds, &writefds, NULL, &tv);
-    if (val_select < 0) {
-        LOG(Error) << "[tcp] select error" << endl;
-        graceful_return("select", E_SELECT);
-    }
-    else if (val_select == 0) {
-        LOG(Error) << "[tcp] tcp select time out" << endl;
-        graceful_return("tcp select time out", E_TIMEOUT);
-    }
-    /*
-	else if (FD_ISSET(socketfd, &readfds) && FD_ISSET(socketfd, &writefds)) {
-        FD_ZERO(&readfds);
-        FD_ZERO(&writefds);
-		close(socketfd);
-        LOG(Error) << "[tcp] peer offline" << endl;
-		graceful_return("peer offline", E_PEEROFF);
-	}
-    */
-    else if (FD_ISSET(socketfd, &writefds)){
-        FD_CLR(socketfd, &writefds);
-        LOG(Debug) << "[tcp] ready to send" << endl;
-        FD_ZERO(&writefds);
-        return ALL_GOOD;
-    }
-    else {
-        LOG(Error) << "[tcp] not permitted to send" << endl;
-        graceful_return("not permitted to send", E_NOSEND);
-    }
-}
-
-int ready_to_recv(int socketfd) {
-    fd_set readfds;
-    struct timeval tv;
-    FD_ZERO(&readfds);
-    FD_SET(socketfd, &readfds);
-    tv.tv_sec = timeout_seconds;
-    tv.tv_usec = timeout_microseconds;
-    errno = 0;
-    int val_select = select(socketfd+1, &readfds, NULL, NULL, &tv);
-    //int val_select = select(socketfd+1, &readfds, NULL, NULL, NULL);
-    if (val_select < 0) {
-        LOG(Error) << "[tcp] select error" << endl;
-        graceful_return("select", E_SELECT);
-    }
-    else if (val_select == 0) {
-        LOG(Error) << "[tcp] time out and no change" << endl;
-        graceful_return("time out and no change", E_TIMEOUT);
-    }
-    else if (FD_ISSET(socketfd, &readfds)){
-        FD_CLR(socketfd, &readfds);
-        LOG(Debug) << "[tcp] ready to recv" << endl;
-        FD_ZERO(&readfds);
-        return ALL_GOOD;
-    }
-    else {
-        LOG(Error) << "[tcp] not permitted to recv" << endl;
-        graceful_return("not permitted to recv", E_NORECV);
-    }
-}
-
-
 Status sender_physical_layer(int *pipefd_down, int *pipefd_up) {
     prctl(PR_SET_PDEATHSIG, SIGHUP);
     fd_set rfds, wfds;
@@ -1717,6 +1330,77 @@ Status receiver_physical_layer(int *pipefd_down, int *pipefd_up) {
     return TRANSMISSION_END;
 }
 
+int ready_to_send(int socketfd) {
+    fd_set readfds, writefds;
+    struct timeval tv;
+    FD_ZERO(&readfds);
+    FD_ZERO(&writefds);
+    //FD_SET(socketfd, &readfds);
+    FD_SET(socketfd, &writefds);
+    tv.tv_sec = timeout_seconds;
+    tv.tv_usec = timeout_microseconds;
+    errno = 0;  // clear errno
+
+    int val_select = select(socketfd+1, &readfds, &writefds, NULL, &tv);
+    if (val_select < 0) {
+        LOG(Error) << "[tcp] select error" << endl;
+        graceful_return("select", E_SELECT);
+    }
+    else if (val_select == 0) {
+        LOG(Error) << "[tcp] tcp select time out" << endl;
+        graceful_return("tcp select time out", E_TIMEOUT);
+    }
+    /*
+	else if (FD_ISSET(socketfd, &readfds) && FD_ISSET(socketfd, &writefds)) {
+        FD_ZERO(&readfds);
+        FD_ZERO(&writefds);
+		close(socketfd);
+        LOG(Error) << "[tcp] peer offline" << endl;
+		graceful_return("peer offline", E_PEEROFF);
+	}
+    */
+    else if (FD_ISSET(socketfd, &writefds)){
+        FD_CLR(socketfd, &writefds);
+        LOG(Debug) << "[tcp] ready to send" << endl;
+        FD_ZERO(&writefds);
+        return ALL_GOOD;
+    }
+    else {
+        LOG(Error) << "[tcp] not permitted to send" << endl;
+        graceful_return("not permitted to send", E_NOSEND);
+    }
+}
+
+int ready_to_recv(int socketfd) {
+    fd_set readfds;
+    struct timeval tv;
+    FD_ZERO(&readfds);
+    FD_SET(socketfd, &readfds);
+    tv.tv_sec = timeout_seconds;
+    tv.tv_usec = timeout_microseconds;
+    errno = 0;
+    int val_select = select(socketfd+1, &readfds, NULL, NULL, &tv);
+    //int val_select = select(socketfd+1, &readfds, NULL, NULL, NULL);
+    if (val_select < 0) {
+        LOG(Error) << "[tcp] select error" << endl;
+        graceful_return("select", E_SELECT);
+    }
+    else if (val_select == 0) {
+        LOG(Error) << "[tcp] time out and no change" << endl;
+        graceful_return("time out and no change", E_TIMEOUT);
+    }
+    else if (FD_ISSET(socketfd, &readfds)){
+        FD_CLR(socketfd, &readfds);
+        LOG(Debug) << "[tcp] ready to recv" << endl;
+        FD_ZERO(&readfds);
+        return ALL_GOOD;
+    }
+    else {
+        LOG(Error) << "[tcp] not permitted to recv" << endl;
+        graceful_return("not permitted to recv", E_NORECV);
+    }
+}
+
 list<T_time_seq_nr> timer_list;
 // template <class T, class t>
 // typename T::iterator find_by_second_in_list(const T &l, const t second) {
@@ -1738,4 +1422,3 @@ void _start_timer(seq_nr k) {
 void _stop_timer(seq_nr k) {
     timer_list.remove_if([&k](const T_time_seq_nr &el) { return el.second == k; } );
 }
-
